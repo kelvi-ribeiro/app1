829b66d0b9e97757dc8e089fc94c24e1fd3db0fd
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2017-11-16 05:05:23 -0200

    Entendo os vários de jeitos de associar o componente ao template

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,13 @@ import{Component} from '@angular/core/'
 
 @Component({
     selector: 'app-topo',
-    templateUrl: './topo.component.html'
-   })
+    
+    //template:'<p>Esse é o componente topo</p>' ESSE É OUTRO JEITO DE ASSOCIOAR O TEMPLATE, MÉTODO INLINE, SEM QUEBRA DE LINHA
+/*template:`<p>Esse
+é o
+Componente
+Topo</p>` // Esse é outro método, usando crases(shift+`) podendo-se ter quebras de linha, facilitando a visualização do código*/
+templateUrl: './topo.component.html'  //ESSE É UM JEITO DE ASSOCIAR O TEMPLATE AO COMPONENT, COM ARQUIVO EXTERNO 
+})
    export class TopoComponent {
    }
