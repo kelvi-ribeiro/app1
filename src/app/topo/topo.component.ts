365357c306b9877f00c2902b46e6c506baf8907e
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2017-11-16 07:24:17 -0200

    Entendendo os vários jeitos de como definir o seletor

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,10 @@
 import{Component} from '@angular/core/'
 
 @Component({
-    selector: 'app-topo',
+    selector: 'app-topo', // Método para definir o seletor da aplicação, que será instanciado na view
+    //seletor :'[app-topo]', Outro método para definir o seletor, dessa vez, como atributo.
+   //seletor :'.app-topo' Outro método para definir o seletor, dessa vez, como classe. 
+    
     
     //template:'<p>Esse é o componente topo</p>' ESSE É OUTRO JEITO DE ASSOCIOAR O TEMPLATE, MÉTODO INLINE, SEM QUEBRA DE LINHA
 /*template:`<p>Esse
