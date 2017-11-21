ec843e17cfe52e746a03ceeed16fbdeb70b4cc6f
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2017-11-20 23:40:06 -0200

    Criando a classe frase no diretório shared.

---
+++ src/app/topo/shared/frase.model.ts
@@ -0,0 +1,8 @@
+export class Frase{
+    //public fraseEng:string Podemos delclarar os atributos
+    //public frasePtBr:string  a partir dos construtores.
+    constructor(public fraseEng:string, public frasePtBr:string){
+        //this.fraseEng = fraseEng   Declarando os atributos no construtor, 
+        //this.frasePtBr = frasePtBr essa linha de código não será nessária
+    }
+}
